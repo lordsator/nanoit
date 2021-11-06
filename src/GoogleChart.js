a07ab85d9a0be3b07b6b122dbb78fd06e432ff25
Alex Gutjahr <alex@alexgutjahr.com>
2021-11-06 11:19:45 +0100

    Change totals to line chart

--- src/GoogleChart.js
+++ src/GoogleChart.js
@@ -8,13 +8,15 @@ const GoogleChart = (props) => {
     <Chart
       width="100%"
       height={"400px"}
-      chartType="AreaChart"
+      chartType="ComboChart"
       loader={<div>Loading Chart</div>}
       data={chart}
       options={{
         title: title,
         legend: { position: "top", maxLines: 3 },
         chartArea: { width: "100%", height: "70%" },
+        seriesType: "area",
+        series: { 0: { type: "line" }, 1: { type: "line" } },
       }}
     />
   );
