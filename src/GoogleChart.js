2368ac6938b2482f2ee31268b54f60eafdc1e012
Alex Gutjahr <alex@alexgutjahr.com>
2021-11-06 14:15:07 +0100

    Stack charts

--- src/GoogleChart.js
+++ src/GoogleChart.js
@@ -16,6 +16,7 @@ const GoogleChart = (props) => {
         legend: { position: "top", maxLines: 3 },
         chartArea: { width: "100%", height: "70%" },
         seriesType: "area",
+        isStacked: true,
         series: { 0: { type: "line" }, 1: { type: "line" } },
       }}
     />
